f262355b9a0ee84021b9d1b3d8b70dbb3aa20e85
SaraHQ <sara.holmquist@gmail.com>
2022-09-07 19:14:28 +0200

    Removed HTTP redirection

--- API/Startup.cs
+++ API/Startup.cs
@@ -44,7 +44,7 @@ namespace API
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             app.UseRouting();
 

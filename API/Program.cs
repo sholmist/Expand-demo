515cd1a91439936fda5557b37af5aa2b89eee45d
SaraHQ <sara.holmquist@gmail.com>
2022-09-10 22:10:30 +0200

    Added seed data to database

--- API/Program.cs
+++ API/Program.cs
@@ -14,23 +14,26 @@ namespace API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var host = CreateHostBuilder(args).Build();
             using var scope = host.Services.CreateScope();
             var services = scope.ServiceProvider;
+            
+            var logger = services.GetRequiredService<ILogger<Program>>();
+
             try
             {
                 var context = services.GetRequiredService<StoreContext>();
-                context.Database.Migrate();
+                await context.Database.MigrateAsync();
+                await StoreContextSeed.SeedAsync(context, logger);
             }
-            catch (System.Exception ex)
+            catch (Exception ex)
             {
-                var logger = services.GetRequiredService<ILogger<Program>>();
                 logger.LogError(ex, ": An error occured during migration");
             }
 
-            host.Run();
+            await host.RunAsync();
         }
 
 

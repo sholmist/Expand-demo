5b4da03c90a07ddc96fcc7cdf7edc0db124ba784
SaraHQ <sara.holmquist@gmail.com>
2022-09-18 08:05:56 +0200

    Added hamburger menu for smaller devices

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -1,14 +1,34 @@
-import exp from "constants";
-import React from "react";
+// import exp from "constants";
+import React, {useState} from "react";
 import * as FaIcon from "react-icons/fa";
 /*TODO Placeholder change src later*/
 import Logo from "../assets/placeholder_logo.png";
 
 const Navigation = () => {
+
+  const [sidebar, setSidebar] = useState(false);
+
+  const showSidebar = () => setSidebar(!sidebar);
+
   return (
     <div className="nav-container">
       <div className="nav">
         <div className="nav__left">
+          <div className="nav__left__hamburger">
+            <FaIcon.FaBars onClick={showSidebar}/>
+            <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+              <ul className="nav-menu-items" onClick={showSidebar}>
+                <li className="cancel">
+                  <FaIcon.FaChevronLeft/>
+                </li>
+                <li className="nav-menu-items__header">
+                  Navigation
+                </li>
+                <li>Categories</li>
+                <li>Courses</li>
+              </ul>
+            </nav>
+          </div>
           <img className="nav__left__logo" src={Logo} alt="logo" />
           <ul className="nav__left__list">
             <div className="nav__left__list__item">Categories</div>

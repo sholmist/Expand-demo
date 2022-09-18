73ba9f670504a3ad8f67e65d69e7184ddb675b01
SaraHQ <sara.holmquist@gmail.com>
2022-09-18 15:27:59 +0200

    New migration and updated database

--- Infrastructure/Migrations/20220918132617_CourseConfiguration.Designer.cs
+++ Infrastructure/Migrations/20220918132617_CourseConfiguration.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Infrastructure.Migrations
 {
     [DbContext(typeof(StoreContext))]
-    partial class StoreContextModelSnapshot : ModelSnapshot
+    [Migration("20220918132617_CourseConfiguration")]
+    partial class CourseConfiguration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

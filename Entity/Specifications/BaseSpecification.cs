afd75a7c8e05641c1f4d8c70c0284677591b07f5
SaraHQ <sara.holmquist@gmail.com>
2022-09-19 14:57:34 +0200

    Created base specification pattern

---
+++ Entity/Specifications/BaseSpecification.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Entity.Specifications
+{
+    public class BaseSpecification<T> : ISpecification<T>
+    {
+        public BaseSpecification(Expression<Func<T, bool>> criteria)
+        {
+            Criteria = criteria;
+        }
+
+        public Expression<Func<T, bool>> Criteria { get; }
+        public List<Expression<Func<T, object>>> Include { get; } = new List<Expression<Func<T, object>>>();
+
+        protected void AddInclude(Expression<Func<T, object>> expression)
+        {
+            Include.Add(expression);
+        }
+    }
+}

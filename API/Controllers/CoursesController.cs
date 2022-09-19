190a42e876dfedb968240d12bc0b3a8a8dffce20
SaraHQ <sara.holmquist@gmail.com>
2022-09-19 12:51:56 +0200

    Injected automapper in controller

--- API/Controllers/CoursesController.cs
+++ API/Controllers/CoursesController.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using API.DTO;
+using AutoMapper;
 using Entity;
 using Entity.Interfaces;
 using Infrastructure;
@@ -12,24 +14,27 @@ namespace API.Controllers
     public class CoursesController : BaseController
     {
         private readonly ICourseRepository _repository;
-        public CoursesController(ICourseRepository repository)
+        private readonly IMapper _mapper;
+        public CoursesController(ICourseRepository repository, IMapper mapper)
         {
+            _mapper = mapper;
             _repository = repository;
         }
 
         [HttpGet]
 
-        public async Task<ActionResult<List<Course>>> GetCourses()
+        public async Task<ActionResult<List<CourseDto>>> GetCourses()
         {
-            var courses =  await _repository.GetCoursesAsync();
-            return Ok(courses);
+            var courses = await _repository.GetCoursesAsync();
+            return Ok(_mapper.Map<IReadOnlyList<Course>, IReadOnlyList<CourseDto>>(courses));
         }
 
         [HttpGet("{id}")]
 
-        public async Task<ActionResult<Course>> GetCourse(Guid id)
+        public async Task<ActionResult<CourseDto>> GetCourse(Guid id)
         {
-            return await _repository.GetCourseByIdAsync(id);
+            var course = await _repository.GetCourseByIdAsync(id);
+            return _mapper.Map<Course, CourseDto>(course);
         }
 
     }

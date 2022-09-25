c4244438bf008b658af14d75cadb5480839a2796
SaraHQ <sara.holmquist@gmail.com>
2022-09-25 12:33:30 +0200

    Removed unused dependencies

--- client/src/components/Categories.tsx
+++ client/src/components/Categories.tsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
-import agent from "../actions/agent";
 import { Category } from "../models/category";
 import {
   categoriesSelector,

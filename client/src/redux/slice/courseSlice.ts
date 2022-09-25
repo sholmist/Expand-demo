fc7a85009895c508b85dd6ddb0dab03404268ef1
SaraHQ <sara.holmquist@gmail.com>
2022-09-25 10:08:58 +0200

    Changed setCoursesAsync to getCoursesAsync

--- client/src/redux/slice/courseSlice.ts
+++ client/src/redux/slice/courseSlice.ts
@@ -6,19 +6,20 @@ import {
 import agent from "../../actions/agent";
 import { Course } from "../../models/course";
 import { PaginatedCourse } from "../../models/paginatedCourse";
+import { RootState } from "../store/configureStore";
 
 const coursesAdapter = createEntityAdapter<Course>();
 
-const setCoursesAsync = createAsyncThunk<PaginatedCourse | undefined, void>(
-  "course/setCoursesAsync",
-  async () => {
-    try {
-      return await agent.Courses.list();
-    } catch (error) {
-      console.log(error);
-    }
+export const getCoursesAsync = createAsyncThunk<
+  PaginatedCourse | undefined,
+  void
+>("course/getCoursesAsync", async () => {
+  try {
+    return await agent.Courses.list();
+  } catch (err) {
+    console.log(err);
   }
-);
+});
 
 export const courseSlice = createSlice({
   name: "course",
@@ -29,15 +30,19 @@ export const courseSlice = createSlice({
   reducers: {},
   extraReducers: (builder) => {
     builder
-      .addCase(setCoursesAsync.pending, (state) => {
+      .addCase(getCoursesAsync.pending, (state) => {
         state.status = "pending";
       })
-      .addCase(setCoursesAsync.fulfilled, (state, action) => {
+      .addCase(getCoursesAsync.fulfilled, (state, action) => {
         coursesAdapter.setMany(state, action.payload!.data);
         state.status = "idle";
       })
-      .addCase(setCoursesAsync.rejected, (state) => {
+      .addCase(getCoursesAsync.rejected, (state) => {
         state.status = "idle";
       });
   },
 });
+
+export const courseSelector = coursesAdapter.getSelectors(
+  (state: RootState) => state.course
+);

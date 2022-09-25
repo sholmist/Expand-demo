da8c6cb03225f7c766201db4a6af8982b86be989
SaraHQ <sara.holmquist@gmail.com>
2022-09-25 12:50:43 +0200

    Added search filters

--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -1,9 +1,17 @@
 import React, { useEffect } from "react";
-import { Row } from "antd";
+import { Card, Col, Radio, Row } from "antd";
 import ShowCourses from "../components/ShowCourses";
 import { Course } from "../models/course";
 import { useAppDispatch, useAppSelector } from "../redux/store/configureStore";
 import { coursesSelector, getCoursesAsync } from "../redux/slice/courseSlice";
+import { categoriesSelector } from "../redux/slice/categorySlice";
+import { Category } from "../models/category";
+
+const sortOptions = [
+  { value: "title", label: "Alphabetical" },
+  { value: "priceDescending", label: "Price - High to Low" },
+  { value: "priceAscending", label: "Price - Low to High" },
+];
 
 const Homepage = () => {
   const courses = useAppSelector(coursesSelector.selectAll);
@@ -14,6 +22,16 @@ const Homepage = () => {
     if (!coursesLoaded) dispatch(getCoursesAsync());
   }, [dispatch, coursesLoaded]);
 
+  const categories = useAppSelector(categoriesSelector.selectAll);
+
+  const getCategories = () => {
+    const categoryArray: any[] = [];
+    categories.forEach((category: Category) => {
+      categoryArray.push({ value: category.id, label: category.name });
+    });
+    return categoryArray;
+  };
+
   return (
     <div className="course">
       <div className="course__header">
@@ -21,11 +39,22 @@ const Homepage = () => {
         <h2>New courses picked just for you...</h2>
       </div>
       <Row gutter={[24, 32]}>
-        {courses &&
-          courses.map((course: Course, index: number) => {
-            return <ShowCourses key={index} course={course} />;
-          })}
-        ;
+        <Col span={4}>
+          <Card title="Sorting Options">
+            <Radio.Group options={sortOptions} />
+          </Card>
+          <Card title="Choose category">
+            <Radio.Group options={getCategories()} />
+          </Card>
+        </Col>
+        <Col span={20}>
+          <Row gutter={[24, 32]}>
+            {courses &&
+              courses.map((course: Course, index: number) => {
+                return <ShowCourses key={index} course={course} />;
+              })}
+          </Row>
+        </Col>
       </Row>
     </div>
   );

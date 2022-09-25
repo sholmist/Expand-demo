98cf2654fbbfadfbaf951fa50ec0db49eed64b74
SaraHQ <sara.holmquist@gmail.com>
2022-09-25 10:15:41 +0200

    Using Selectors in Entity Adapters

--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -1,35 +1,34 @@
-import React, {useState, useEffect} from "react";
-import {Row} from "antd";
-import agent from "../actions/agent";
-import { PaginatedCourse } from "../models/paginatedCourse";
+import React, { useEffect } from "react";
+import { Row } from "antd";
 import ShowCourses from "../components/ShowCourses";
 import { Course } from "../models/course";
+import { useAppDispatch, useAppSelector } from "../redux/store/configureStore";
+import { courseSelector, getCoursesAsync } from "../redux/slice/courseSlice";
 
 const Homepage = () => {
-    const [data, setData ] = useState<PaginatedCourse>();
+  const courses = useAppSelector(courseSelector.selectAll);
+  const dispatch = useAppDispatch();
+  const { coursesLoaded } = useAppSelector((state) => state.course);
 
-    useEffect(() => {
+  useEffect(() => {
+    if (!coursesLoaded) dispatch(getCoursesAsync());
+  }, [dispatch, coursesLoaded]);
 
-        agent.Courses.list().then(response => {
-            setData(response);
-        })
-    }, [])
-
-
-
-    return (
-        <div className="course">
-            <div className="course__header">
-                <h1>What to learn next?</h1>
-                <h2>New courses picked just for you...</h2>
-            </div>
-            <Row gutter={[24, 32]}>
-                {data && data.data.map((course: Course, index: number) => {
-                    return <ShowCourses key={index} course={course} />
-                })};
-            </Row>
-        </div>
-    );
+  return (
+    <div className="course">
+      <div className="course__header">
+        <h1>What to learn next?</h1>
+        <h2>New courses picked just for you...</h2>
+      </div>
+      <Row gutter={[24, 32]}>
+        {courses &&
+          courses.map((course: Course, index: number) => {
+            return <ShowCourses key={index} course={course} />;
+          })}
+        ;
+      </Row>
+    </div>
+  );
 };
 
 export default Homepage;

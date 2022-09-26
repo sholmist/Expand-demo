e2e061482a196d31968dc5f3e81482b4ddcd96a3
SaraHQ <sara.holmquist@gmail.com>
2022-09-26 16:30:53 +0200

    Set up frontend for identity

--- client/src/actions/agent.ts
+++ client/src/actions/agent.ts
@@ -3,6 +3,7 @@ import { Basket } from "../models/basket";
 import { Category } from "../models/category";
 import { Course } from "../models/course";
 import { PaginatedCourse } from "../models/paginatedCourse";
+import { Login, Register } from "../models/user";
 
 axios.defaults.baseURL = "http://localhost:5000/api";
 
@@ -17,6 +18,11 @@ const requests = {
     axios.post<T>(url, body).then(responseBody),
   put: <T>(url: string, body: {}) => axios.put<T>(url, body).then(responseBody),
   del: <T>(url: string) => axios.delete<T>(url).then(responseBody),
+};
+
+const Users = {
+  login: (values: Login) => requests.post("/user/login", values),
+  register: (values: Register) => requests.post("/user/register", values),
 };
 
 const Courses = {
@@ -42,6 +48,7 @@ const agent = {
   Courses,
   Categories,
   Baskets,
+  Users,
 };
 
 export default agent;

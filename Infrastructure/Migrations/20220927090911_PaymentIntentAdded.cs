39a1a33847daf4b1e11ce7105e423d42185fca1c
SaraHQ <sara.holmquist@gmail.com>
2022-09-27 11:11:29 +0200

    New migration

---
+++ Infrastructure/Migrations/20220927090911_PaymentIntentAdded.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Migrations
+{
+    public partial class PaymentIntentAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "88d33567-b8b1-4419-8dc9-5427c4a73cbe");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "9323b8f6-f87d-4a9d-9e5e-543dc06ce8b3");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ClientSecret",
+                table: "Baskets",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentIntentId",
+                table: "Baskets",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "d5209a95-d163-4b69-b4c8-bd3f497a6d8f", "aa9a30d6-0233-43e5-ba0d-2039a2af54e5", "Student", "STUDENT" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "c016ac83-4a31-490b-9c6e-5a3b7566fa97", "e2b078a5-9cb4-471f-8987-e20d69260c78", "Instructor", "INSTRUCTOR" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "c016ac83-4a31-490b-9c6e-5a3b7566fa97");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "d5209a95-d163-4b69-b4c8-bd3f497a6d8f");
+
+            migrationBuilder.DropColumn(
+                name: "ClientSecret",
+                table: "Baskets");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentIntentId",
+                table: "Baskets");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "9323b8f6-f87d-4a9d-9e5e-543dc06ce8b3", "2d928081-b2f2-47eb-8ad4-b6983d188af2", "Student", "STUDENT" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "88d33567-b8b1-4419-8dc9-5427c4a73cbe", "87f2f04a-389a-4992-ba8b-b10cc23c5096", "Instructor", "INSTRUCTOR" });
+        }
+    }
+}
